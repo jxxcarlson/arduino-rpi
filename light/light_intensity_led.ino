8061f2273d11b68bfbddb09e4863d034e813cab6
James Carlson <jxxcarlson@mac.com>
2020-04-01 16:44:19 +0200

    Create light_intensity_led.ino

---
+++ light/light_intensity_led.ino
@@ -0,0 +1,21 @@
+int sensorValue ;
+int threshold;
+
+void setup () {
+   Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue = analogRead(A1);
+  threshold = 225;
+  Serial.println(sensorValue);
+  if (sensorValue < threshold) {
+    pinMode(A1, OUTPUT);
+    digitalWrite(A1, HIGH);
+    tone(9, 2900, 500);
+    delay(1000);
+    pinMode(A1, INPUT);
+  }
+  delay(50);
+}
+
