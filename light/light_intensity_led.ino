20de2874f109d05681ad196255b5395adf16f82f
James Carlson <jxxcarlson@mac.com>
2020-04-01 16:48:52 +0200

    Update light_intensity_led.ino

--- light/light_intensity_led.ino
+++ light/light_intensity_led.ino
@@ -1,5 +1,6 @@
 // [Reference (YouTube video)](https://www.youtube.com/watch?v=jIkQAUCc7Fk)int sensorValue ;
                                                    
+int sensorValue ;
 int threshold;
 
 void setup () {
@@ -8,13 +9,13 @@ void setup () {
 
 void loop() {
   sensorValue = analogRead(A1);
-  threshold = 225;
+  threshold = 215;
   Serial.println(sensorValue);
   if (sensorValue < threshold) {
     pinMode(A1, OUTPUT);
     digitalWrite(A1, HIGH);
-    tone(9, 2900, 500);
-    delay(1000);
+    tone(9, 2900, 150);
+    delay(200);
     pinMode(A1, INPUT);
   }
   delay(50);
